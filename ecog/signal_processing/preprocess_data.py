64496ceb9d2804d21a451204e49e5de8ac39d530
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2019-05-29 07:49:05 -0700

    * uncomment high gammaa bands * only compute if not present * print done

--- ecog/signal_processing/preprocess_data.py
+++ ecog/signal_processing/preprocess_data.py
@@ -58,7 +58,9 @@ def transform(block_path, filter='default', bands_vals=None):
     if filter=='default':
         band_param_0 = bands.chang_lab['cfs']
         band_param_1 = bands.chang_lab['sds']
-    #elif filter=='high_gamma':
+    elif filter=='high_gamma':
+        band_param_0 = bands.chang_lab['cfs'][(bands.chang_lab['cfs'] > 70) & (bands.chang_lab['cfs'] < 150)]
+        band_param_1 = bands.chang_lab['sds'][(bands.chang_lab['cfs'] > 70) & (bands.chang_lab['cfs'] < 150)]
         #band_param_0 = [ bands.neuro['min_freqs'][-1] ]  #for hamming window filter
         #band_param_1 = [ bands.neuro['max_freqs'][-1] ]
         #band_param_0 = bands.chang_lab['cfs'][29:37]      #for average of gaussian filters
@@ -68,92 +70,86 @@ def transform(block_path, filter='default', bands_vals=None):
         band_param_1 = bands_vals[1,:]
 
 
-    subj_path, block_name = os.path.split(block_path)
     block_name = os.path.splitext(block_path)[0]
 
     start = time.time()
 
-    with NWBHDF5IO(block_path, 'r+') as io:
+    with NWBHDF5IO(block_path, 'a') as io:
         nwb = io.read()
-        # 1e6 scaling helps with numerical accuracy
-        X = nwb.acquisition['ECoG'].data[:].T * 1e6
-        fs = nwb.acquisition['ECoG'].rate
-        bad_elects = load_bad_electrodes(nwb)
-        print('Load time for h5 {}: {} seconds'.format(block_name,
-                                                       time.time() - start))
-        print('rates {}: {} {}'.format(block_name, rate, fs))
-        if not np.allclose(rate, fs):
-            assert rate < fs
-            X = resample(X, rate, fs)
-
-        if bad_elects.sum() > 0:
-            X[bad_elects] = np.nan
-
-        # Subtract CAR
-        start = time.time()
-        X = subtract_CAR(X)
-        print('CAR subtract time for {}: {} seconds'.format(block_name,
-                                                            time.time()-start))
-
-        # Apply Notch filters
-        start = time.time()
-        X = linenoise_notch(X, rate)
-        print('Notch filter time for {}: {} seconds'.format(block_name,
-                                                            time.time()-start))
-
-        # Apply Hilbert transform
-        X = X.astype('float32')   #signal (nChannels,nSamples)
-        nChannels = X.shape[0]
-        nSamples = X.shape[1]
-        nBands = len(band_param_0)
-        Xp = np.zeros((nBands, nChannels, nSamples))  #power (nBands,nChannels,nSamples)
-        X_fft_h = None
-        for ii, (bp0, bp1) in enumerate(zip(band_param_0, band_param_1)):
-            #if filter=='high_gamma':
-            #    kernel = hamming(X, rate, bp0, bp1)
-            #else:
-            kernel = gaussian(X, rate, bp0, bp1)
-            X_analytic, X_fft_h = hilbert_transform(X, rate, kernel, phase=None, X_fft_h=X_fft_h)
-            Xp[ii] = abs(X_analytic).astype('float32')
-
-        # Scales signals back to Volt
-        X /= 1e6
-
 
         # Storage of processed signals on NWB file -----------------------------
-        try:      # if ecephys module already exists
-            ecephys_module = nwb.modules['ecephys']
-        except:   # creates ecephys ProcessingModule
-            ecephys_module = ProcessingModule(name='ecephys',
-                                              description='Extracellular electrophysiology data.')
+        if 'ecephys' not in nwb.modules:
             # Add module to NWB file
-            nwb.add_processing_module(ecephys_module)
+            nwb.create_processing_module(name='ecephys', description='Extracellular electrophysiology data.')
+        ecephys_module = nwb.modules['ecephys']
 
 
         # LFP: Downsampled and power line signal removed
-        try:    # if LFP already exists
-            lfp = nwb.modules['ecephys'].data_interfaces['LFP']
+        if 'LFP' in nwb.modules['ecephys'].data_interfaces:
             lfp_ts = nwb.modules['ecephys'].data_interfaces['LFP'].electrical_series['preprocessed']
-        except: # creates LFP data interface container
+            X = lfp_ts.data[:].T
+            rate = lfp_ts.rate
+        else:
+
+            # 1e6 scaling helps with numerical accuracy
+            X = nwb.acquisition['ECoG'].data[:].T * 1e6
+            fs = nwb.acquisition['ECoG'].rate
+            bad_elects = load_bad_electrodes(nwb)
+            print('Load time for h5 {}: {} seconds'.format(block_name,
+                                                           time.time() - start))
+            print('rates {}: {} {}'.format(block_name, rate, fs))
+            if not np.allclose(rate, fs):
+                assert rate < fs
+                X = resample(X, rate, fs)
+
+            if bad_elects.sum() > 0:
+                X[bad_elects] = np.nan
+
+            # Subtract CAR
+            start = time.time()
+            X = subtract_CAR(X)
+            print('CAR subtract time for {}: {} seconds'.format(block_name,
+                                                                time.time() - start))
+
+            # Apply Notch filters
+            start = time.time()
+            X = linenoise_notch(X, rate)
+            print('Notch filter time for {}: {} seconds'.format(block_name,
+                                                                time.time() - start))
+
+
             lfp = LFP()
             # Add preprocessed downsampled signals as an electrical_series
-            elecs_region = nwb.electrodes.create_region(name='electrodes',
-                                                        region=np.arange(nChannels).tolist(),
-                                                        description='')
             lfp_ts = lfp.create_electrical_series(name='preprocessed',
                                                   data=X.T,
-                                                  electrodes=elecs_region,
+                                                  electrodes=nwb.acquisition['ECoG'].electrodes,
                                                   rate=rate,
                                                   description='')
             ecephys_module.add_data_interface(lfp)
 
 
         # Spectral band power
-        try:     # if decomposition already exists
-            dec = nwb.modules['ecephys'].data_interfaces['Bandpower_'+filter]
-            write_file = 0
-        except:  # creates DecompositionSeries
-            # bands: (DynamicTable) frequency bands that signal was decomposed into
+        if 'Bandpower_'+filter  not in nwb.modules['ecephys'].data_interfaces:
+
+            # Apply Hilbert transform
+            X = X.astype('float32')  # signal (nChannels,nSamples)
+            nChannels = X.shape[0]
+            nSamples = X.shape[1]
+            nBands = len(band_param_0)
+            Xp = np.zeros((nBands, nChannels, nSamples))  # power (nBands,nChannels,nSamples)
+            X_fft_h = None
+            for ii, (bp0, bp1) in enumerate(zip(band_param_0, band_param_1)):
+                # if filter=='high_gamma':
+                #    kernel = hamming(X, rate, bp0, bp1)
+                # else:
+                kernel = gaussian(X, rate, bp0, bp1)
+                X_analytic, X_fft_h = hilbert_transform(X, rate, kernel, phase=None, X_fft_h=X_fft_h)
+                Xp[ii] = abs(X_analytic).astype('float32')
+
+            # Scales signals back to Volt
+            X /= 1e6
+
+
             band_param_0V = VectorData(name='filter_param_0',
                               description='frequencies for bandpass filters',
                               data=band_param_0)
@@ -176,6 +172,6 @@ def transform(block_path, filter='default', bands_vals=None):
                                         rate=rate,
                                         source_timeseries=lfp_ts)
             ecephys_module.add_data_interface(decs)
+        io.write(nwb)
 
-        if write_file==1: # if new fields are created
-            io.write(nwb)
+        print('done', flush=True)
